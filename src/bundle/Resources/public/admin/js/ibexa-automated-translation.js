8dd1b2b54a308df5826048d6bce83d1bb324927f
matx132 <mateusz@mateuszdebinski.pl>
2024-03-18 15:40:05 +0100

    Corrected CS

--- src/bundle/Resources/public/admin/js/ibexa-automated-translation.js
+++ src/bundle/Resources/public/admin/js/ibexa-automated-translation.js
@@ -1,17 +1,18 @@
-((doc) => {
+((global, doc) => {
     const TRANSLATOR_SELECT_SELECTOR = '#add-translation_translatorAlias';
     const BASE_LANGUAGE_SELECT_SELECTOR = '#add-translation_base_language';
-
     const translatorSelect = doc.querySelector(TRANSLATOR_SELECT_SELECTOR);
     const baseLanguageSelect = doc.querySelector(BASE_LANGUAGE_SELECT_SELECTOR);
+
     if (baseLanguageSelect && translatorSelect) {
         baseLanguageSelect.addEventListener('change', () => {
             translatorSelect.disabled = !baseLanguageSelect.value;
 
             const translationSelectWrapper = translatorSelect.closest('.ibexa-dropdown');
+
             if (translationSelectWrapper) {
                 translationSelectWrapper.classList.toggle('ibexa-dropdown--disabled', !baseLanguageSelect.value);
             }
         });
     }
-}) (document);
+}) (window, document);
