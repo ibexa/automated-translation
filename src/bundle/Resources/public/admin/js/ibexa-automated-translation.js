eb7d9a13d209fdbd742f1e02539874d8c01c199c
Vidar Langseid <vl@ibexa.co>
2025-11-21 13:59:01 +0100

    Fixed so that "automatic translation" checkbox in "Create a new translation" dialog takes supported languages into account

--- src/bundle/Resources/public/admin/js/ibexa-automated-translation.js
+++ src/bundle/Resources/public/admin/js/ibexa-automated-translation.js
@@ -4,17 +4,32 @@
     translationModals.forEach((modal) => {
         const translatorSelect = modal.querySelector('.ibexa-automated-translation-services-container__input');
         const baseLanguageSelect = modal.querySelector('.ibexa-translation__language-wrapper--base-language');
+        const languageSelect = modal.querySelector('.ibexa-translation__language-wrapper--language');
 
-        if (baseLanguageSelect && translatorSelect) {
-            baseLanguageSelect.addEventListener('change', () => {
-                translatorSelect.disabled = !baseLanguageSelect.value;
+        const handleLanguageChange = () => {
+            const translationCheckbox = translatorSelect.closest('.ibexa-input--checkbox');
+            if (translationCheckbox) {
+                const supportedLanguages = translationCheckbox.getAttribute(
+                    `data-supported-translation-languages-${translationCheckbox.value}`
+                );
+                translationCheckbox.disabled = !(
+                    baseLanguageSelect.value  &&
+                    supportedLanguages.includes(baseLanguageSelect.value) &&
+                    supportedLanguages.includes(languageSelect.value)
+                );
+                translationCheckbox.checked = !translationCheckbox.disabled;
 
-                const translationSelectWrapper = translatorSelect.closest('.ibexa-dropdown');
+            }
 
-                if (translationSelectWrapper) {
-                    translationSelectWrapper.classList.toggle('ibexa-dropdown--disabled', !baseLanguageSelect.value);
-                }
-            });
+            const translationSelectWrapper = translatorSelect.closest('.ibexa-dropdown');
+            if (translationSelectWrapper) {
+                translationSelectWrapper.classList.toggle('ibexa-dropdown--disabled', !baseLanguageSelect.value);
+            }
+        };
+
+        if (baseLanguageSelect && languageSelect && translatorSelect) {
+            baseLanguageSelect.addEventListener('change', handleLanguageChange);
+            languageSelect.addEventListener('change', handleLanguageChange);
         }
     });
-})(document);
+}) (document);
